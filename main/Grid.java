be6cb7c990c73c3534ada9eeaee09330cfe01dbc
nfsmith <nickfsmith96@gmail.com>
2014-03-31 14:06:59 -0400

    commit

--- main/Grid.java
+++ main/Grid.java
@@ -20,10 +20,10 @@ public class Grid {
 	public Grid(GameScreen gameScreen) {
 		this.gameScreen = gameScreen;
 		sideLength = gameScreen.getWidth() / xBoxes;
-		generateSimpleMaze();
+		generateRandomMaze();
 		placeBases();
-		hBlobs.add(new Blob(100, 3, this, false));
-		cBlobs.add(new Blob(100, 3, this, true));
+		// hBlobs.add(new Blob(100, 3, this, false));
+		// cBlobs.add(new Blob(100, 3, this, true));
 	}
 
 	private void generateSimpleMaze() {
@@ -35,6 +35,99 @@ public class Grid {
 			maze[i][9] = true;
 			maze[i][8] = true;
 			maze[i][7] = true;
+		}
+	}
+
+	private void generateRandomMaze() {
+		maze = new boolean[xBoxes][yBoxes];
+		boolean[][] offLimits = new boolean[xBoxes][yBoxes];
+		for (int x = 0; x < xBoxes; x++) {
+			for (int y = 0; y < yBoxes; y++) {
+				maze[x][y] = false;
+				offLimits[x][y] = false;
+			}
+		}
+
+		ArrayList<Point> path = new ArrayList<Point>();
+		int rand = (int) (4 * Math.random());
+		if (rand == 0) {
+			rand = (int) ((xBoxes - 2) * Math.random()) + 1;
+			maze[rand][0] = true;
+			path.add(new Point(rand, 0));
+			offLimits[rand][0] = true;
+			offLimits[rand - 1][0] = true;
+			offLimits[rand + 1][0] = true;
+		} else if (rand == 1) {
+			rand = (int) ((yBoxes - 2) * Math.random()) + 1;
+			maze[xBoxes - 1][rand] = true;
+			path.add(new Point(xBoxes - 1, rand));
+			offLimits[xBoxes - 1][rand] = true;
+			offLimits[xBoxes - 1][rand - 1] = true;
+			offLimits[xBoxes - 1][rand + 1] = true;
+		} else if (rand == 2) {
+			rand = (int) ((xBoxes - 2) * Math.random()) + 1;
+			maze[rand][yBoxes - 1] = true;
+			path.add(new Point(rand, yBoxes - 1));
+			offLimits[rand][yBoxes - 1] = true;
+			offLimits[rand - 1][yBoxes - 1] = true;
+			offLimits[rand + 1][yBoxes - 1] = true;
+		} else {
+			rand = (int) ((yBoxes - 2) * Math.random()) + 1;
+			maze[0][rand] = true;
+			path.add(new Point(0, rand));
+			offLimits[0][rand] = true;
+			offLimits[0][rand - 1] = true;
+			offLimits[0][rand + 1] = true;
+		}
+		boolean finished = false;
+		while (!finished) {
+			rand = (int) (4 * Math.random());
+			if (rand == 0) {
+				int x = (int) path.get(path.size() - 1).getX();
+				int y = (int) path.get(path.size() - 1).getY() - 1;
+				if (y >= 0 && !offLimits[x][y]) {
+					maze[x][y] = true;
+					path.add(new Point(x, y));
+					offLimits[x][y + 1] = true;
+					offLimits[x - 1][y + 1] = true;
+					offLimits[x + 1][y + 1] = true;
+				}
+			} else if (rand == 1) {
+				int x = (int) path.get(path.size() - 1).getX() + 1;
+				int y = (int) path.get(path.size() - 1).getY();
+				if (x <= xBoxes - 1 && !offLimits[x][y]) {
+					maze[x][y] = true;
+					path.add(new Point(x, y));
+					offLimits[x - 1][y] = true;
+					offLimits[x - 1][y - 1] = true;
+					offLimits[x - 1][y + 1] = true;
+				}
+			} else if (rand == 2) {
+				int x = (int) path.get(path.size() - 1).getX();
+				int y = (int) path.get(path.size() - 1).getY() + 1;
+				if (y <= yBoxes - 1 && !offLimits[x][y]) {
+					maze[x][y] = true;
+					path.add(new Point(x, y));
+					offLimits[x][y - 1] = true;
+					offLimits[x - 1][y - 1] = true;
+					offLimits[x + 1][y - 1] = true;
+				}
+			} else {
+				int x = (int) path.get(path.size() - 1).getX() - 1;
+				int y = (int) path.get(path.size() - 1).getY();
+				if (x >= 0 && !offLimits[x][y]) {
+					maze[x][y] = true;
+					path.add(new Point(x, y));
+					offLimits[x + 1][y] = true;
+					offLimits[x + 1][y - 1] = true;
+					offLimits[x + 1][y + 1] = true;
+				}
+			}
+			int x = (int) path.get(path.size() - 1).getX();
+			int y = (int) path.get(path.size() - 1).getY();
+			if ((x == 0 || x == xBoxes - 1 || y == 0 || y == yBoxes - 1)&&path.size()>1) {
+				finished = true;
+			}
 		}
 	}
 
@@ -90,16 +183,16 @@ public class Grid {
 	}
 
 	public void tick() {
-		for (Blob b : cBlobs){
-			if (b.touchesBase()){
+		for (Blob b : cBlobs) {
+			if (b.touchesBase()) {
 				cBlobs.remove(b);
 				hBase.damage(10);
 				return;
 			}
 			b.tick();
 		}
-		for (Blob b : hBlobs){
-			if (b.touchesBase()){
+		for (Blob b : hBlobs) {
+			if (b.touchesBase()) {
 				hBlobs.remove(b);
 				cBase.damage(10);
 				return;
@@ -120,12 +213,12 @@ public class Grid {
 			g.drawLine(x * sideLength, 0, x * sideLength, sideLength * yBoxes);
 		for (int y = 0; y <= yBoxes; y++)
 			g.drawLine(0, y * sideLength, sideLength * xBoxes, y * sideLength);
-		hBase.draw(g, sideLength);
-		cBase.draw(g, sideLength);
-		for (Blob b : cBlobs)
-			b.draw(g);
-		for (Blob b : hBlobs)
-			b.draw(g);
+		// hBase.draw(g, sideLength);
+		// cBase.draw(g, sideLength);
+		// for (Blob b : cBlobs)
+		// b.draw(g);
+		// for (Blob b : hBlobs)
+		// b.draw(g);
 	}
 
 	public Base getHBase() {
