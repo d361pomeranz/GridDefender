eebd0aa80824b3fe8eb88bf7fea693cde488104c
nfsmith <nickfsmith96@gmail.com>
2014-05-13 13:36:16 -0400

    bike

--- main/EndScreen.java
+++ main/EndScreen.java
@@ -1,15 +1,20 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 
 
 public class EndScreen extends GDScreen{
 	private String name;
+	private Button exitButton;
 	public EndScreen(GDFrame frame, String s) {
 		super(frame);
 		name=s;
+		exitButton = new Button(frame.getWidth() - 30, 0, 30, 30, "X");
+		exitButton.setBGColor(Color.red);
+		exitButton.setTextColor(Color.white);
 	}
 
 	@Override
@@ -47,7 +52,10 @@ public class EndScreen extends GDScreen{
 		Graphics g = getFrame().getBufferStrategy().getDrawGraphics();
 		g.setColor(Color.gray);
 		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.black);
+		g.setFont(new Font("Impact", 64, 32));
 		g.drawString("This nigga "+name+" just won yo", 100, 100);
+		exitButton.draw(g);
 		
 		
 	}
